92471a82d476670176d3a54768ea46d81bee834a
Martin Boßlet <Martin.Bosslet@gmail.com>
2012-11-25 02:39:55 +0100

    Add binyo-missing.h

---
+++ ext/binyo/binyo-missing.h
@@ -0,0 +1,19 @@
+/*
+* binyo - Fast binary IO for Ruby
+*
+* Copyright (C) 2012
+* Martin Bosslet <martin.bosslet@googlemail.com>
+* All rights reserved.
+*
+* See the file 'LICENSE' for further details about licensing.
+*/
+
+#ifndef _BINYO_MISSING_H_
+#define _BINYO_MISSING_H_
+
+#ifndef HAVE_RB_IO_CHECK_BYTE_READABLE 
+#define rb_io_check_byte_readable(fptr)		rb_io_check_readable(fptr)
+#endif
+
+#endif /* BINYO_MISSING_H */
+
